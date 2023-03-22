96b5135ee39496f4e110ba38314e5fee8f1e57a9
Bong DeGuzman <79473084+BDevelopment1@users.noreply.github.com>
2023-03-22 18:28:07 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+Config.PedFrequency = 0.0     -- Alters the ped and traffic frequency. Values MUST be between 1.0 and 0.0 (1.0 > x > 0.0)
+Config.TrafficFrequency = 0.0 -- For example: 0.4 is valid but 1.6 or 5.0 is NOT valid. -- Leave both to 0.0 to disable all NPC.
+
+Config.RagdollWhenShot = true -- enables player ragdoll (fall into ground) when shot.
+
+
+Config.DisableVehicleRewards = true -- Prevent getting items when riding a vehicle. For example, getting new gun when entering a police car.
+
+Config.Trains = true -- Freight Train and metro travelling around the city.
+
+Config.SameHealthForMaleAndFemalesPeds = true -- sets both male and female health to 200 (100 in game).
+
+Config.NoAutoHealthRegen = true -- disable auto health regen
+
